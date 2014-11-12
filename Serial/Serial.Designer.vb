32cca320ff89ea20d8004ded5a3a99f8df811a5e
BJ98BJ <bj98bj@outlook.com>
2014-11-12 18:06:33 +0100

    1

---
+++ Serial/Serial.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Serial
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(71, 4)
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(34, 20)
+        Me.NumericUpDown1.TabIndex = 0
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 6)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Com -Port"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 100.0!)
+        Me.TextBox1.Location = New System.Drawing.Point(13, 40)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(484, 209)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(131, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 100.0!)
+        Me.TextBox2.Location = New System.Drawing.Point(503, 40)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(463, 209)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 700
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(213, 6)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox1.TabIndex = 5
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(301, 3)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Serial
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(978, 595)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Name = "Serial"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Serial/Serial.vb
@@ -0,0 +1,109 @@
+﻿Imports System
+Imports System.IO.Ports
+Imports System.Threading
+Imports System.Threading.Thread
+
+Public Class Serial
+
+    Dim WithEvents COMport As New SerialPort
+    Dim RXbyte As Byte
+    Dim CurrentCom As Integer = 1
+    Dim dist As Integer
+    Dim entf As Integer
+    Dim count As Integer
+    Dim currdist As Integer
+
+    Private Sub Serial_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    'Function ReceiveSerialData(Port As Integer) As String
+    '    ' Receive strings from a serial port.
+    '    Dim returnStr As String = ""
+
+    '    Dim com1 As IO.Ports.SerialPort = Nothing
+    '    Try
+    '        com1 = My.Computer.Ports.OpenSerialPort("COM" & Port)
+    '        com1.ReadTimeout = 10000
+    '        Do
+    '            Dim Incoming As String = com1.ReadLine()
+    '            If Incoming Is Nothing Then
+    '                Exit Do
+    '            Else
+    '                returnStr &= Incoming & vbCrLf
+    '            End If
+    '        Loop
+    '    Catch ex As TimeoutException
+    '        returnStr = "Error: Serial Port read timed out."
+    '    Catch exx As Exception
+    '        returnStr = "Keine Daten vorhanden."
+    '    Finally
+    '        If com1 IsNot Nothing Then com1.Close()
+    '    End Try
+
+    '    Return returnStr
+    'End Function
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        TextBox1.Text = entf
+        TextBox2.Text = currdist
+        If entf < 50 And Not entf = 0 Then
+            Timer2.Enabled = True
+            Me.BackColor = Color.Red
+            If CheckBox1.Checked = True Then
+                Process.Start(TextBox3.Text)
+            End If
+        Else
+            Timer2.Enabled = False
+            My.Computer.Audio.Stop()
+            Me.BackColor = Color.DarkGreen
+        End If
+        'TextBox1.Text = ReceiveSerialData(NumericUpDown1.Value)
+    End Sub
+
+    Public Sub New()
+
+        ' Dieser Aufruf ist für den Designer erforderlich.
+        InitializeComponent()
+
+        ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
+        COMport.PortName = "COM4"
+        COMport.BaudRate = 9600
+        COMport.Open()
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        COMport.PortName = "COM" & NumericUpDown1.Value
+    End Sub
+
+    'Private Sub Receiver(ByVal sender As Object, ByVal e As SerialDataReceivedEventArgs) Handles COMport.DataReceived
+    '    RXbyte = COMport.ReadByte
+    '    Me.Invoke(New MethodInvoker(AddressOf Display))
+
+    'End Sub
+
+    Private Sub COMport_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles COMport.DataReceived
+        Dim lese As Integer = COMport.ReadLine()
+        If count = 5 Then
+            count = 0
+            entf = dist / 5
+            dist = 0
+        Else
+            dist = dist + lese
+            count = count + 1
+        End If
+        currdist = lese
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form1.Show()
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        My.Computer.Audio.Play(My.Resources.SIREN2, AudioPlayMode.Background)
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+    End Sub
+End Class
